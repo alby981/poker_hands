2e74042687ea87137d397aaaea440fd23adbd788
Alby981 <albertopuntobelotti@gmail.com>
2020-08-14 16:44:17 +0200

    some refactoring... needs more job. It's overkill right now

--- app/PokerHand/PlayingCard/PlayingCard.php
+++ app/PokerHand/PlayingCard/PlayingCard.php
@@ -7,84 +7,66 @@ namespace App\PokerHand\PlayingCard;
  */
 class PlayingCard {
 
-  public $card;
-  public $suit;
-  public $value;
-  public $rank;
+    public $card;
+    public $suit;
+    public $value;
+    public $rank;
+    static public $suits = array('S', 'H', 'D', 'C');
+    static public $card_order = array(
+        2 => 2,
+        3 => 3,
+        4 => 4,
+        5 => 5,
+        6 => 6,
+        7 => 7,
+        8 => 8,
+        9 => 9,
+        10 => 10,
+        'J' => 11,
+        'Q' => 12,
+        'K' => 13,
+        'A' => 14,
+    );
 
-  static public $suits = array('S', 'H', 'D', 'C');
+    static public function createFromString($value = '') {
+        if (empty($value)) {
+            throw new \Exception('Card is empty.');
+        }
 
-  static public $card_order = array(
-    2 => 2,
-    3 => 3,
-    4 => 4,
-    5 => 5,
-    6 => 6,
-    7 => 7,
-    8 => 8,
-    9 => 9,
-    10 => 10,
-    'J' => 11,
-    'Q' => 12,
-    'K' => 13,
-    'A' => 14,
-  );
+        $value_length = strlen($value);
 
-  static public function createFromString($value = '') {
-    if (empty($value)) {
-      throw new \Exception('Card is empty.');
+        if ($value_length < 2 || $value_length > 3) {
+            throw new \Exception('Card does not have a valid string representation.');
+        }
+
+        $suit = substr($value, -1);
+
+        $card_value = substr($value, 0, $value_length - strlen($suit));
+
+        $card = new static();
+
+        $card->value = $card_value;
+        $card->suit = $suit;
+        $card->card = $value;
+        $card->rank = -1;
+
+        return $card;
     }
 
-    $value_length = strlen($value);
+    static public function compare(PlayingCard $a, PlayingCard $b) {
+        $suit_rank = array_flip(self::$suits);
 
-    if ($value_length < 2 || $value_length > 3) {
-      throw new \Exception('Card does not have a valid string representation.');
+        if (self::$card_order[$a->value] > self::$card_order[$b->value] ||
+                (self::$card_order[$a->value] == self::$card_order[$b->value] &&
+                $suit_rank[$a->suit] < $suit_rank[$b->suit])) {
+            return true;
+        }
+
+        return false;
     }
 
-    $suit = substr($value, -1);
-
-    if (!self::validateSuit($suit)) {
-      throw new \Exception('Card does not have a valid suit: ' . $suit);
+    static public function equal(PlayingCard $a, PlayingCard $b) {
+        return self::$card_order[$a->value] == self::$card_order[$b->value];
     }
-
-    $card_value = substr($value, 0, $value_length - strlen($suit));
-
-    if (!self::validateValue($card_value)) {
-      throw new \Exception('Card does not have a valid value: ' . $card_value);
-    }
-
-    $card = new static();
-
-    $card->value = $card_value;
-    $card->suit = $suit;
-    $card->card = $value;
-    $card->rank = -1;
-
-    return $card;
-  }
-
-  static public function validateValue($value) {
-    return in_array($value, array_keys(self::$card_order));
-  }
-
-  static public function validateSuit($suit) {
-    return in_array($suit, self::$suits);
-  }
-
-  static public function compare(PlayingCard $a, PlayingCard $b) {
-    $suit_rank = array_flip(self::$suits);
-
-    if (self::$card_order[$a->value] > self::$card_order[$b->value] ||
-        (self::$card_order[$a->value] == self::$card_order[$b->value] &&
-         $suit_rank[$a->suit] < $suit_rank[$b->suit])) {
-      return TRUE;
-    }
-
-    return FALSE;
-  }
-
-  static public function equal(PlayingCard $a, PlayingCard $b) {
-    return self::$card_order[$a->value] == self::$card_order[$b->value];
-  }
 
 }
